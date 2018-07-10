4c8530b00f7b33d7f1937b0d0ea45b6ef1862f42
Frank Morrison <fmorrison42@gmail.com>
2018-07-10 15:06:10 -0500

    Update WinDefend-Update.bat

--- WinDefend-Update.bat
+++ WinDefend-Update.bat
@@ -1,1 +1,13 @@
 
+
+gpupdate /force
+
+sc config WinDefend start= auto
+
+net start WinDefend
+
+cd %ProgramFiles%\Windows Defender
+MpCmdRun.exe -removedefinitions -dynamicsignatures
+MpCmdRun.exe -SignatureUpdate
+
+Pause
