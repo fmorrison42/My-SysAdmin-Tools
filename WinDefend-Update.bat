c4fd26e62498237c4843e2b6e2ea746d63e3ac28
Frank Morrison <fmorrison42@gmail.com>
2018-07-11 16:37:38 -0500

    added WUAUSERV restart

--- WinDefend-Update.bat
+++ WinDefend-Update.bat
@@ -1,13 +1,16 @@
-
-
 gpupdate /force
 
 sc config WinDefend start= auto
-
 net start WinDefend
+
+net stop wuauserv
+net start wuauserv
 
 cd %ProgramFiles%\Windows Defender
 MpCmdRun.exe -removedefinitions -dynamicsignatures
 MpCmdRun.exe -SignatureUpdate
 
-Pause
+net stop WinDefend
+sc config WinDefend start= disabled
+
+REM pause
