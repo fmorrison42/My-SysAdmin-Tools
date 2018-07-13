c9b767748882146fa132e16ce97b1c1ee7059159
Frank Morrison <fmorrison42@gmail.com>
2018-07-13 14:07:47 -0500

    Added in the Date print-out for the status file

--- PSExec-WinDefendUpdate.bat
+++ PSExec-WinDefendUpdate.bat
@@ -1,6 +1,11 @@
 cd c:\pstools
 
 @echo off
+
+@echo off
+For /f "tokens=2-4 delims=/ " %a in ('date /t') do (set mydate=%c-%a-%b) 
+echo Computers Updated on %mydate% >> "\\apps\install_files\User Scripts\WindowsDefenderUpdate\WinDefend-Update-List.txt" 
+
 for /f %G in (computers.txt) do (
 echo processing computer %G
 psexec \\%G -u XXXXXXX -p XXXXXXX cmd.exe /c "\WinDefForceUpdate.bat" )
