97f140d01ce0313c059102e85d6685d767f658ea
Frank Morrison <fmorrison42@gmail.com>
2018-07-18 07:36:05 -0500

    Added Output and status info
    I realize there's probably a WAY better way to do this, but currently, it works.

--- WinDefend-Update.bat
+++ WinDefend-Update.bat
@@ -1,16 +1,40 @@
+
+echo Running WinDefend Update on %computername%
+
+echo Computer Status for: %computername% >> "\\apps\install_files\User Scripts\WindowsDefenderUpdate\WinDefend-Update-List.txt"
+echo -------------------- >> "\\apps\install_files\User Scripts\WindowsDefenderUpdate\WinDefend-Update-List.txt"
 gpupdate /force
 
-sc config WinDefend start= auto
-net start WinDefend
+echo Windows Defender Start Status: >> "\\apps\install_files\User Scripts\WindowsDefenderUpdate\WinDefend-Update-List.txt"
+sc config WinDefend start= auto 
+net start WinDefend >> "\\apps\install_files\User Scripts\WindowsDefenderUpdate\WinDefend-Update-List.txt"
 
-net stop wuauserv
-net start wuauserv
+echo __ >> "\\apps\install_files\User Scripts\WindowsDefenderUpdate\WinDefend-Update-List.txt"
 
+echo WSUS Restart Status: >> "\\apps\install_files\User Scripts\WindowsDefenderUpdate\WinDefend-Update-List.txt"
+net stop wuauserv >> "\\apps\install_files\User Scripts\WindowsDefenderUpdate\WinDefend-Update-List.txt"
+net start wuauserv >> "\\apps\install_files\User Scripts\WindowsDefenderUpdate\WinDefend-Update-List.txt"
+
+echo __ >> "\\apps\install_files\User Scripts\WindowsDefenderUpdate\WinDefend-Update-List.txt"
 cd %ProgramFiles%\Windows Defender
-MpCmdRun.exe -removedefinitions -dynamicsignatures
-MpCmdRun.exe -SignatureUpdate
+echo Remove Definitions Output: >> "\\apps\install_files\User Scripts\WindowsDefenderUpdate\WinDefend-Update-List.txt"
+MpCmdRun.exe -removedefinitions -dynamicsignatures >> "\\apps\install_files\User Scripts\WindowsDefenderUpdate\WinDefend-Update-List.txt"
 
-net stop WinDefend
-sc config WinDefend start= disabled
+echo __ >> "\\apps\install_files\User Scripts\WindowsDefenderUpdate\WinDefend-Update-List.txt"
+echo Signature Update Output:  >> "\\apps\install_files\User Scripts\WindowsDefenderUpdate\WinDefend-Update-List.txt"
+MpCmdRun.exe -SignatureUpdate >> "\\apps\install_files\User Scripts\WindowsDefenderUpdate\WinDefend-Update-List.txt"
+echo+  >> "\\apps\install_files\User Scripts\WindowsDefenderUpdate\WinDefend-Update-List.txt"
 
-REM pause
+echo __ >> "\\apps\install_files\User Scripts\WindowsDefenderUpdate\WinDefend-Update-List.txt"
+echo Windows Defender Shutdown Status: >> "\\apps\install_files\User Scripts\WindowsDefenderUpdate\WinDefend-Update-List.txt"
+net stop WinDefend 
+sc config WinDefend start= disabled >> "\\apps\install_files\User Scripts\WindowsDefenderUpdate\WinDefend-Update-List.txt"
+
+
+echo+  >> "\\apps\install_files\User Scripts\WindowsDefenderUpdate\WinDefend-Update-List.txt"
+
+REM echo %computername% WinDefend Update Complete >> "\\apps\install_files\User Scripts\WindowsDefenderUpdate\WinDefend-Update-List.txt"
+
+echo ############## >> "\\apps\install_files\User Scripts\WindowsDefenderUpdate\WinDefend-Update-List.txt"
+echo, >> "\\apps\install_files\User Scripts\WindowsDefenderUpdate\WinDefend-Update-List.txt"
+echo= >> "\\apps\install_files\User Scripts\WindowsDefenderUpdate\WinDefend-Update-List.txt"
